251de296e77123e9988c3bc94418bf0e23bc1d34
Sri Pakalapati <srivalli.pakalapati@gmail.com>
2026-07-16 23:44:59 +0100

    added mochaewsome reporting

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,12 +3,29 @@ import { defineConfig } from "cypress";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+const mochawesomeReporter = require("cypress-mochawesome-reporter/plugin");
+
 export default defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    reportFilename: "test-report",
+    reportPageTitle: "Toolshop Automation Test Report",
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    overwrite: true,
+    html: false,
+    json: true,
+    code: false
+  },
   e2e: {
     specPattern: "cypress/e2e/**/*.feature",
     baseUrl: 'https://practicesoftwaretesting.com/',
     async setupNodeEvents(on, config) {
-      await addCucumberPreprocessorPlugin(on, config); on("file:preprocessor", createBundler({
+      await addCucumberPreprocessorPlugin(on, config);
+      mochawesomeReporter(on);
+      on("file:preprocessor", createBundler({
         plugins: [createEsbuildPlugin(config)],
       }));
       return config;
